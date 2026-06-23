a37cb70f2359435afe5977773e4b46d157d8dd1e
Bhuvan <bhuvaneshwarankumar2000@gamil.com>
2026-06-23 16:43:17 +0530

    Check auto deployment in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <h1>MERN Stack App</h1>
+      <h1>We are creating a simple TODO app using MERN stack. Checking if the auto deployment is happening correctly. in vercel</h1>
     </div>
   )
 }
